7a52060b8d3757a6850619ec16bac16cfebe4d93
Jackson <jack-ten@hotmail.com>
2020-11-07 23:13:05 -0300

    melhorias na navegação

--- apps/app-web/src/app/modules/pagenotfound/page/pagenotfound/pagenotfound.component.ts
+++ apps/app-web/src/app/modules/pagenotfound/page/pagenotfound/pagenotfound.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Select } from '@ngxs/store';
+import { ProdutoState } from 'apps/app-web/src/app/data/store/state';
+import { Produto } from 'libs/data/src/lib/classes';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'personalizados-lopes-pagenotfound',
@@ -6,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagenotfound.component.scss']
 })
 export class PagenotfoundComponent implements OnInit {
-
+  @Select(ProdutoState.ObterListaProdutos) Produtos$: Observable<Produto[]>;
   constructor() { }
 
   ngOnInit(): void {
