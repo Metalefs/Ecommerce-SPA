4af467f828ed23e08ecb6b642d572272a897f1c7
Hermes <hermes@localhost.localdomain>
2020-12-04 00:28:59 -0300

    implementando visualizaçao e edicao  de cores e tamanhos ao produto

--- apps/app-web/src/app/modules/gerenciamento/page/editar-produto/DialogComponents/editar-dialog/editar-dialog.component.ts
+++ apps/app-web/src/app/modules/gerenciamento/page/editar-produto/DialogComponents/editar-dialog/editar-dialog.component.ts
@@ -1,5 +1,10 @@
-import { Component, Inject, Input, OnInit } from '@angular/core';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import { Component, ElementRef, Inject, OnInit, ViewChild,Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatAutocomplete, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { MatChipInputEvent } from '@angular/material/chips';
 
+import { Observable } from 'rxjs';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { entities } from '@personalizados-lopes/data';
 import { CategoriaService } from 'apps/app-web/src/app/data/service';
@@ -11,6 +16,24 @@ import { Produto } from 'libs/data/src/lib/classes';
   styleUrls: ['./editar-dialog.component.scss']
 })
 export class EditarProdutoDialogComponent implements OnInit {
+  fileNames:string="nenhum arquivo selecionado.";
+  visible = true;
+  selectable = true;
+  removable = true;
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+
+  colorCtrl = new FormControl();
+  filteredColors: Observable<string[]>;
+  allColors: string[] = ['white', 'green', 'orange', 'red', 'black', 'blue', 'yellow'];
+
+  sizeCtrl = new FormControl();
+  filteredSizes: Observable<string[]>;
+  allSizes: string[] = ['P','M','G','GG','XGG'];
+
+  @ViewChild('colorInput') colorInput: ElementRef<HTMLInputElement>;
+  @ViewChild('tamanhoInput') tamanhoInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto1') matAutocompleteCor: MatAutocomplete;
+  @ViewChild('auto2') matAutocompleteTamanho: MatAutocomplete;
 
   @Input() Produto:Produto;
   Categorias: entities.Categoria[];
@@ -32,6 +55,57 @@ export class EditarProdutoDialogComponent implements OnInit {
   upload($event){
     this.Produto.FileList = $event.target.files;
     console.log(this.Produto.FileList);
+  }
+
+  addCor(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+    if ((value || '').trim())
+      this.Produto.Cores.push(value.trim());
+    if (input)
+      input.value = '';
+
+    this.colorCtrl.setValue(null);
+  }
+  addTamanho(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+    if ((value || '').trim())
+      this.Produto.Tamanhos.push(value.trim());
+    if (input)
+      input.value = '';
+
+    this.sizeCtrl.setValue(null);
+  }
+  removeCor(color: string): void {
+    const index = this.Produto.Cores.indexOf(color);
+    if (index >= 0) {
+      this.Produto.Cores.splice(index, 1);
+    }
+  }
+  removeTamanho(tamanho: string): void {
+    const index = this.Produto.Tamanhos.indexOf(tamanho);
+    if (index >= 0) {
+      this.Produto.Tamanhos.splice(index, 1);
+    }
+  }
+
+
+  selectedCor(event: MatAutocompleteSelectedEvent): void {
+    this.Produto.Cores.push(event.option.viewValue);
+    this.colorInput.nativeElement.value = '';
+    this.colorCtrl.setValue(null);
+  }
+  selectedTamanho(event: MatAutocompleteSelectedEvent): void {
+    this.Produto.Tamanhos.push(event.option.viewValue);
+    this.colorInput.nativeElement.value = '';
+    this.colorCtrl.setValue(null);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.allColors.filter(fruit => fruit.toLowerCase().indexOf(filterValue) === 0);
   }
 
   SelecionarCategoria($event){
