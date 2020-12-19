cd96523e37b485e90cc7c0f9cc58c10a277c6d5d
Hermes <hermes@localhost.localdomain>
2020-12-19 16:14:13 -0300

    Melhorias inicio

--- apps/app-web/src/app/modules/inicio/page/header-footer/header-footer.component.ts
+++ apps/app-web/src/app/modules/inicio/page/header-footer/header-footer.component.ts
@@ -16,15 +16,6 @@ export class HeaderFooterComponent implements OnInit {
       console.log(x);
       this.Cards = [
         {
-          title:"Galeria",
-          icon:"group_work",
-          color:"#FD6D13",
-          class:"red",
-          link:"/showcase",
-          id:"#SHOWCASE",
-          content:"Recebemos pedidos personalizados para a arte do seu produto."
-        },
-        {
           title:"Produtos",
           icon:"photo_library",
           color:"#FFB041",
@@ -33,6 +24,15 @@ export class HeaderFooterComponent implements OnInit {
           id:"#PRODUTO",
           content:`Confira nossas canecas, camisetas, almofadas e outros.`,
           img_src:x.filter(x=>x.Nome == "GIF Produtos")[0].Src
+        },
+        {
+          title:"Galeria",
+          icon:"group_work",
+          color:"#FD6D13",
+          class:"red",
+          link:"/showcase",
+          id:"#SHOWCASE",
+          content:"Recebemos pedidos personalizados para a arte do seu produto."
         },
         {
           title:"Contato",
