815209376db210accfe533b32e3eba59055ab854
Hermes <hermes@localhost.localdomain>
2020-12-29 10:15:08 -0300

    melhorias navegaçao

--- apps/app-web/src/app/modules/blog/page/listagem-posts/listagem-posts.component.ts
+++ apps/app-web/src/app/modules/blog/page/listagem-posts/listagem-posts.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'apps/app-web/src/app/core/service/authentication/authentication.service';
 import { BlogPostService } from 'apps/app-web/src/app/data/service';
-import { BlogPost } from 'libs/data/src/lib/classes';
+import { BlogPost, Usuario } from 'libs/data/src/lib/classes';
+import { StatusPostagem } from 'libs/data/src/lib/classes/blogPost';
+import { TipoUsuario } from 'libs/data/src/lib/enums';
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -12,7 +15,9 @@ export class ListagemPostsComponent implements OnInit {
 
   Blog:BlogPost[];
   loading:boolean = true;
-  constructor(private BlogService:BlogPostService) { }
+  statusPostagem = StatusPostagem;
+  user:Usuario;
+  constructor(private BlogService:BlogPostService,private authService:AuthenticationService) { }
 
   ngOnInit(): void {
     this.BlogService.getAll().snapshotChanges().pipe(
@@ -25,6 +30,15 @@ export class ListagemPostsComponent implements OnInit {
       this.Blog = data;
       this.loading = false;
     });
+    this.authService.currentUser.subscribe(x=>{
+      this.user = x;
+    })
   }
-
+  CanView(post:BlogPost){
+    if(post.StatusPostagem == this.statusPostagem.aberto)
+      return true;
+    else if((this.user?.Tipo || TipoUsuario.normal) == TipoUsuario.admin)
+      return true;
+    return false;
+  }
 }
