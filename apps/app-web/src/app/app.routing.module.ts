7a5e4f0d5816b1101d7e71b9d70ef095676337d9
Hermes <hermes@localhost.localdomain>
2021-02-01 11:30:04 -0300

    footer dark

--- apps/app-web/src/app/app.routing.module.ts
+++ apps/app-web/src/app/app.routing.module.ts
@@ -2,18 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LayoutModule } from './layout/layout.module';
-import { GerenciamentoModule } from './modules/gerenciamento/gerenciamento.module';
-import { LoginModule } from './modules/login/login.module';
 import { UnderConstructionModule } from './modules/under-construction/under-construction.module';
-import { InicioModule } from './modules/inicio/inicio.module';
-import { EmpresaModule } from './modules/empresa/empresa.module';
-import { ServicoModule } from './modules/servico/servico.module';
-import { ShowcaseModule } from './modules/showcase/showcase.module';
-import { ProdutosModule } from './modules/produtos/produtos.module';
-import { OrcamentoModule } from './modules/orcamento/orcamento.module';
-import { CheckoutModule } from './modules/checkout/checkout.module';
-import { BlogModule } from './modules/blog/blog.module';
-import { MinhaContaModule } from './modules/minha-conta/minha-conta.module';
 
 import { PagenotfoundModule } from './modules/pagenotfound/pagenotfound.module';
 
@@ -32,7 +21,7 @@ const routes: Routes = [
     loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule)
   },
   {
-    path: 'gerenciamento/app',
+    path: 'gerenciamento',
     loadChildren: () => import('./modules/gerenciamento/gerenciamento.module').then(m => m.GerenciamentoModule)
   },
   {
--- apps/app-web/src/app/modules/gerenciamento/gerenciamento.routing.ts
+++ apps/app-web/src/app/modules/gerenciamento/gerenciamento.routing.ts
@@ -21,7 +21,7 @@ import { OrcamentoDetailComponent } from './page/editar-orcamento/orcamento-deta
 
 export const routes: Routes = [
   {
-    path: "app",
+    path: '',
     component: GerenciamentoComponent,
     canActivate: [AuthGuard],
     children: [
