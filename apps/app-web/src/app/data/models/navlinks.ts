39cc58cc4f6dbd9fdb3ddd8bf23d871752be49d0
Hermes <hermes@localhost.localdomain>
2021-03-08 12:41:35 -0300

    melhorias nav

--- apps/app-web/src/app/data/models/navlinks.ts
+++ apps/app-web/src/app/data/models/navlinks.ts
@@ -23,6 +23,7 @@ export interface NavLinkOptions{
 
 export let NavLinks:NavLink[] =
 environment.production ? [
+  {name: "Inicio",     href:"/",  icon:'',group:GrupoNavLink.institucional},
   {name: "Sobre",     href:"empresa",  icon:'',group:GrupoNavLink.institucional},
   {name: "Serviços",  href:"servicos", icon:'',group:GrupoNavLink.institucional},
   {name: "Básicos",   href:"produtos", icon:'shopping_bag',group:GrupoNavLink.produtos},
@@ -30,6 +31,7 @@ environment.production ? [
   {name: "Blog",      href:"blog",     icon:'comment', group:GrupoNavLink.duvidas},
 ] : [
   {name: "Institucional", href:"", icon:'',group:GrupoNavLink.none},
+  {name: "Inicio",     href:"/",  icon:'',group:GrupoNavLink.institucional},
   {name: "Sobre",     href:"empresa",  icon:'',group:GrupoNavLink.institucional},
   {name: "Serviços",  href:"servicos", icon:'',group:GrupoNavLink.institucional},
   {name: "Básicos",   href:"produtos", icon:'shopping_bag',group:GrupoNavLink.produtos},
@@ -49,19 +51,19 @@ environment.production ? [
       {nome:"Galeria",link:`/showcase`,queryParams:{}},
     ]
   },
-  {name: "Básicos",   href:"produtos", icon:'',group:GrupoNavLink.produtos},
-  {name: "Orcamento",   href:"orcamento",icon:'', group:GrupoNavLink.duvidas},
-  {name: "Blog",      href:"blog",     icon:'', group:GrupoNavLink.duvidas},
+  {name: "Básicos",   href:"produtos", icon:'shopping_bag',group:GrupoNavLink.produtos},
+  {name: "Orcamento",   href:"orcamento",icon:'mail', group:GrupoNavLink.duvidas},
+  {name: "Blog",      href:"blog",     icon:'comment', group:GrupoNavLink.duvidas},
 ] : [
   {
-    name: "Institucional", href:"", icon:'',group:GrupoNavLink.none,
+    name: "Institucional", href:"", icon:'home',group:GrupoNavLink.none,
     options:[
       {nome:"Inicio",link:`/inicio`,queryParams:{}},
       {nome:"Sobre",link:`/empresa`,queryParams:{}},
       {nome:"Serviços",link:`/servicos`,queryParams:{}},
     ]
   },
-  {name: "Básicos",   href:"produtos", icon:'',group:GrupoNavLink.produtos},
-  {name: "Orcamento",   href:"orcamento",icon:'', group:GrupoNavLink.duvidas},
-  {name: "Blog",      href:"blog",     icon:'', group:GrupoNavLink.duvidas},
+  {name: "Básicos",   href:"produtos", icon:'shopping_bag',group:GrupoNavLink.produtos},
+  {name: "Orcamento",   href:"orcamento",icon:'mail', group:GrupoNavLink.duvidas},
+  {name: "Blog",      href:"blog",     icon:'comment', group:GrupoNavLink.duvidas},
 ];
