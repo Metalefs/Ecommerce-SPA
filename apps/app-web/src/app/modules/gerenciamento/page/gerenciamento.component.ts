25fb6e7f61d2eb9470ac16f7ac0637112c46743e
Hermes <jack-ten@hotmail.com>
2021-07-19 18:27:19 -0300

    melhorias visuais

--- apps/app-web/src/app/modules/gerenciamento/page/gerenciamento.component.ts
+++ apps/app-web/src/app/modules/gerenciamento/page/gerenciamento.component.ts
@@ -64,7 +64,7 @@ export class GerenciamentoComponent implements OnInit {
       {name: "Assinantes", href: "emails"},
       {name: "Depoimentos", href: "cliente"},
       {name: "Noticias", href: "blog"},
-      {name: "Imagens do site", href: "imagens"},
+      {name: "Imagens", href: "imagens"},
     ]
   }
   prepareRoute(outlet: RouterOutlet) {
